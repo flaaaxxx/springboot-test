e8b6b57b78ff33729fe670385790123960d63a1a
flaaaxxx <flaaaxxx@gmail.com>
2022-03-28 19:34:33 +0200

    Add e2e test

--- src/test/java/com/training/vehicles/controllers/VehicleControllerE2ETest.java
+++ src/test/java/com/training/vehicles/controllers/VehicleControllerE2ETest.java
@@ -9,7 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -20,7 +25,23 @@ import java.util.Objects;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@AutoConfigureMockMvc // automatyczne skonfigurowanie MockMvc
 class VehicleControllerE2ETest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    TestRestTemplate testRestTemplate;
+
+    @Test
+    public void shouldReturnByColor() {
+
+        ResponseEntity<Vehicle[]> actual = testRestTemplate.exchange("http://localhost:" + port + "/api/vehicles/filter?color=red",
+                HttpMethod.GET,
+                HttpEntity.EMPTY,
+                Vehicle[].class);
+
+        Assertions.assertEquals("Alfa Romeo", Objects.requireNonNull(actual.getBody())[0].getBrand());
+    }
 
 }
